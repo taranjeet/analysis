4dd6c2a94fba9b5037977e4ba0d54babf19a4e1f
root <deshrajdry@gmail.com>
2015-04-13 02:32:06 +0530

    all types of problems fetched

--- codechef/models.py
+++ codechef/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Easy(models.Model):
+    code = models.CharField(max_length=10,primary_key=True)
+    name = models.CharField(max_length=500)
+
+class Medium(models.Model):
+    code = models.CharField(max_length=10,primary_key=True)
+    name = models.CharField(max_length=500)
+
+class Hard(models.Model):
+    code = models.CharField(max_length=10,primary_key=True)
+    name = models.CharField(max_length=500)
+
+class Challenge(models.Model):
+    code = models.CharField(max_length=10,primary_key=True)
+    name = models.CharField(max_length=500)
+
+class Peer(models.Model):
+    code = models.CharField(max_length=10,primary_key=True)
+    name = models.CharField(max_length=500)
+
+class School(models.Model):
+    code = models.CharField(max_length=10,primary_key=True)
+    name = models.CharField(max_length=500)
